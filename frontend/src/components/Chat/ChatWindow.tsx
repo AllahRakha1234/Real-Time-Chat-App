f2e6482cf6c6fe79ee6ecbaf87293aa45d7c5e58
AllahRakha1234 <allahrakhahsp1234@gmail.com>
2025-10-13 23:04:33 +0500

    feat: socket integrated

--- frontend/src/components/Chat/ChatWindow.tsx
+++ frontend/src/components/Chat/ChatWindow.tsx
@@ -1,28 +1,73 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMessageStore } from "@/store/message.store";
+import { useChatStore } from "@/store/chat.store";
 import MessageList from "@/components/Chat/MessageList";
 import MessageInput from "@/components/Chat/MessageInput";
 import { Loader } from "@/components/ui/loader";
 import type { User } from "@/types/auth";
+import { io } from "socket.io-client";
+import type { Message } from "@/types/message";
 
-const ChatWindow = ({ chatId, currentUser }: {chatId: string, currentUser: User | null}) => {
-    const {
-        messages,
-        isLoading,
-        error,
-        getAllChatMessages,
-        sendMessage,
-    } = useMessageStore();
-
-    // Fetch messages when a new chat is opened
-    useEffect(() => {
-        if (chatId) getAllChatMessages(chatId);
-    }, [chatId, getAllChatMessages]);
+const ChatWindow = ({ chatId, currentUser }: { chatId: string; currentUser: User | null }) => {
+    const { messages, isLoading, error, getAllChatMessages, sendMessage, addMessage } = useMessageStore();
+    const { selectedChat } = useChatStore();
+    const [socketConnected, setSocketConnected] = useState(false);
+    const socketRef = useRef<any>(null);
 
     const handleSendMessage = async (content: string) => {
         if (!content.trim()) return;
-        await sendMessage(content, chatId);
+        const { message } = await sendMessage(content, chatId);
+        socketRef.current.emit("new-message", message);
     };
+
+    useEffect(() => {
+        socketRef.current = io(import.meta.env.VITE_SERVER_URL || "http://localhost:5000");
+
+        socketRef.current.on("connect", () => {
+            console.log("Connected to Socket.IO server");
+        });
+
+        if (currentUser) socketRef.current.emit("setup", currentUser);
+
+        socketRef.current.on("connected", () => {
+            console.log("Socket setup complete");
+            setSocketConnected(true);
+        });
+
+        return () => {
+            socketRef.current.disconnect();
+            console.log("Disconnected from Socket.IO server");
+        };
+    }, [currentUser]);
+
+    useEffect(() => {
+        if (chatId && socketConnected) {
+            getAllChatMessages(chatId);
+            console.log("chatID:", chatId);
+            socketRef.current.emit("join-chat", chatId);
+        }
+    }, [chatId, socketConnected, getAllChatMessages]);
+
+    useEffect(() => {
+        if (!socketRef.current) return;
+
+        const handleMessageReceived = (newMessage: Message) => {
+            if (newMessage.chat._id === selectedChat?._id) {
+                console.log("New message received:", newMessage);
+                addMessage(newMessage);
+            } else {
+                console.log("New message for another chat:", newMessage);
+            }
+        };
+
+        socketRef.current.on("message-received", handleMessageReceived);
+
+        // Cleanup to prevent multiple listeners
+        return () => {
+            socketRef.current.off("message-received", handleMessageReceived);
+        };
+    }, [selectedChat?._id, addMessage]);
+
 
     if (isLoading)
         return (
@@ -40,12 +85,10 @@ const ChatWindow = ({ chatId, currentUser }: {chatId: string, currentUser: User 
 
     return (
         <div className="relative flex flex-col h-full w-full">
-            {/* Message List */}
             <div className="flex-1 max-h-[68vh] overflow-y-auto bg-gray-50 mt-2">
                 <MessageList messages={messages} currentUser={currentUser} />
             </div>
 
-            {/* Input Field */}
             <div className="flex-shrink-0 border-t-2 border-primary rounded-xl pt-2">
                 <MessageInput onSend={handleSendMessage} />
             </div>
